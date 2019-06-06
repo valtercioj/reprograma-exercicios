e0becfe65ab7aebda1add1480fbb02eb0c9322d6
ninasolon <ninasolon@gmail.com>
2019-06-06 09:56:18 -0300

    outras resolucoes com promise

--- javascript/js4/promises.js
+++ javascript/js4/promises.js
@@ -66,3 +66,59 @@ const returnParImpar = data => {
     .then(res => console.log(res))
     .catch(err => console.log(err))
 }
+
+// (raissa)
+
+const isANumber = numero => {
+    return new Promise((resolve, reject) => {
+        (typeof numero !== 'number') ? reject('deu ruim') : resolve('ok')
+    })
+}
+
+const returnPromise = numero => {
+    return new Promise((resolve, reject) => { 
+        if (numero % 2 === 1) {
+            setTimeout(() => {
+                resolve ('ímpar')
+            }, 1000)
+        } if (numero % 2 === 0) {
+            setTimeout(() => {
+                resolve ('par')
+            }, 2000)
+        }    
+    })  
+}
+
+function job(data) {
+    isANumber(data)
+    .then(() => returnPromise(data))
+    .then(resolve => console.log(resolve))
+    .catch(reject => console.log(reject))
+}
+
+// (mayara)
+
+/* Crio uma função que verifica se é par[true] ou impar[false] */
+const parOuImpar = num => num % 2 === 0 ? true : false;
+
+//crio uma variavel recebendo uma função com um parâmetro 'numero' que tem uma Promise
+const novaPromessa = (numero) => {
+  return new Promise((resolve, reject) => {
+    //crio uma variavel que recebe a função parOuImpar 
+    const numPar = parOuImpar(numero) 
+    //identifico se 'numero' é um número msm e se o parametro esta vazio
+    if(!isNaN(numPar) || numero == null) {
+      reject("Deu ruim")
+    }
+    else {
+        //verifico se 'numPar' é par[true] : impar[false]
+      par === true ? setTimeout(() => {resolve(numero + " é par")}, 2000) : setTimeout(() => {resolve(numero + " é impar")}, 1000)
+    }
+  })  
+}
+
+function retornoPromessa(num){
+ novaPromessa(num)
+ .then(resultado => console.log({resultado}))
+ .catch(reject => console.log({reject}))
+};
